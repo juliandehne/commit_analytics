868ca68db010d95f258ea7b4fb9c702c5fdd9e29
Paul Balluff <paul.balluff@gmail.com>
2024-12-04 13:39:03 +0100

    adjust plot color, load data from parquet file, code formatting

--- word-cooccurrence-network.py
+++ word-cooccurrence-network.py
@@ -1,3 +1,4 @@
+from enum import Enum
 import re
 import itertools
 from collections import Counter
@@ -5,14 +6,15 @@ import networkx as nx
 import matplotlib.pyplot as plt
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+import polars as pl
 
 # Read lines from the file
-with open('commit_messages', 'r', encoding='utf-8') as file:
-    lines = file.readlines()
-
+lines = pl.read_parquet("commits.parquet").get_column("message").to_list()
 
 # Preprocessing
-stop_words = set(stopwords.words('english'))
+# import nltk
+# nltk.download('stopwords')
+stop_words = set(stopwords.words("english"))
 processed_lines = []
 
 
@@ -20,8 +22,9 @@ for line in lines:
     # Tokenize, clean, and normalize
     words = [
         word.lower()
-        for word in word_tokenize(re.sub(r'\W+', ' ', line.strip()))
-        if word.lower() not in stop_words and not word.isdigit()  # Exclude stopwords and numeric tokens
+        for word in word_tokenize(re.sub(r"\W+", " ", line.strip()))
+        if word.lower() not in stop_words
+        and not word.isdigit()  # Exclude stopwords and numeric tokens
     ]
     processed_lines.append(words)
 
@@ -32,7 +35,7 @@ window_size = 2
 co_occurrences = Counter()
 for words in processed_lines:
     for i in range(len(words) - window_size + 1):
-        window = words[i:i + window_size]
+        window = words[i : i + window_size]
         co_occurrences.update(itertools.combinations(sorted(set(window)), 2))
 
 # Create graph
@@ -47,13 +50,26 @@ G.remove_nodes_from(nodes_to_remove)
 # Calculate node degrees for scaling node sizes
 node_sizes = [G.degree(node) * 100 for node in G.nodes()]
 
+# GESIS colors (from website)
+colors = {"purple": (0.39, 0.16, 0.47), "darkblue": (0, 0.23, 0.47)}
+
 # Visualize the network
 plt.figure(figsize=(12, 8))
 pos = nx.spring_layout(G, seed=42)
-nx.draw(G, pos, with_labels=True, node_color="lightblue", node_size=node_sizes, font_size=10)
-weights = nx.get_edge_attributes(G, 'weight')
+nx.draw(
+    G,
+    pos,
+    with_labels=True,
+    node_color=colors["darkblue"],
+    node_size=node_sizes,
+    font_size=10,
+    edgecolors="gray",
+    font_color="whitesmoke",
+    edge_color="whitesmoke",
+)
 
 
-#nx.draw_networkx(G, pos)
+# nx.draw_networkx(G, pos)
 plt.title("Word Co-occurrence Network")
-plt.show()
+# plt.show()
+plt.savefig("commits_words_coocurrence.pdf", facecolor="black", transparent=True)
